743b87a772b5ec1967a59c676c0f659bf80b1ceb
Jucielly Silva <juciellysouzasilva@gmail.com>
2021-06-29 12:35:05 -0300

    feat: note logo component created

--- src/components/pages/TestPage/index.js
+++ src/components/pages/TestPage/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Button from '../../atoms/Button';
 import Input from '../../atoms/Input';
 import BaseCard from '../../atoms/BaseCard';
+import NoteLogo from '../../atoms/NoteLogo';
 
 const TestPage = () => (
   <>
     <Button>Login</Button>
     <Input placeholder="name@emxample.com" />
     <BaseCard />
+    <NoteLogo />
   </>
 );
 
