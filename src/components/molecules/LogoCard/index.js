824214e7a3b3e7dd532aa3b351f956604ba4292e
Jucielly Silva <juciellysouzasilva@gmail.com>
2021-06-29 18:03:38 -0300

    feat: logo card component created

---
+++ src/components/molecules/LogoCard/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Container } from './style';
+import NoteLogo from '../../atoms/NoteLogo';
+
+const LogoCard = () => (
+  <Container>
+    <NoteLogo />
+  </Container>
+);
+
+export default LogoCard;
--- src/components/pages/TestPage/index.js
+++ src/components/pages/TestPage/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { HiOutlineMail } from 'react-icons/hi';
 import TextField from '../../molecules/TextField';
+import LogoCard from '../../molecules/LogoCard';
 
 const TestPage = () => (
   <>
@@ -9,6 +10,7 @@ const TestPage = () => (
       icon={HiOutlineMail}
       color="helper"
     />
+    <LogoCard />
   </>
 );
 
