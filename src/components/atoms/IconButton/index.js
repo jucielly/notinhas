c4ca95fbe57a75e6b5efb134e2e788b6ab619487
Jucielly Silva <juciellysouzasilva@gmail.com>
2021-06-29 14:40:17 -0300

    feat: icon button component created

---
+++ src/components/atoms/IconButton/index.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const IconButton = styled.button`
+  background-color: transparent;
+  box-shadow: none;
+  border: none;
+  font-size: 30px;
+  &:active {
+    outline: none;
+  }
+`;
+
+export default IconButton;
